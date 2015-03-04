51fcb6c0d951a5a0be83e9d6c032771d3d30b8ff
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2015-03-04 11:33:50 +0000

    Add Main.hs

---
+++ src/Main.hs
@@ -0,0 +1,40 @@
+module Main where
+
+import Idris.Core.TT
+import Idris.AbsSyntax
+import Idris.ElabDecls
+import Idris.REPL
+
+import IRTS.Compiler
+import IRTS.CodegenGAP
+
+import System.Environment
+import System.Exit
+
+import Paths_idris_gap
+
+data Opts = Opts { inputs :: [FilePath],
+                   output :: FilePath }
+
+showUsage = do putStrLn "Usage: idris-php <ibc-files> [-o <output-file>]"
+               exitWith ExitSuccess
+
+getOpts :: IO Opts
+getOpts = do xs <- getArgs
+             return $ process (Opts [] "a.php") xs
+  where
+    process opts ("-o":o:xs) = process (opts { output = o }) xs
+    process opts (x:xs) = process (opts { inputs = x:inputs opts }) xs
+    process opts [] = opts
+
+c_main :: Opts -> Idris ()
+c_main opts = do elabPrims
+                 loadInputs (inputs opts) Nothing
+                 mainProg <- elabMain
+                 ir <- compile (Via "gap") (output opts) mainProg
+                 runIO $ codegenGAP ir
+main :: IO ()
+main = do opts <- getOpts
+          if (null (inputs opts))
+             then showUsage
+             else runMain (c_main opts)
